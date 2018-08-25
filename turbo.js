2db465c987742513af0842e7b5afc9d422376b64
acf01 <aaron-friedman@att.net>
2018-08-24 20:24:10 -0700

    Now everything is working! Still need images.

--- turbo.js
+++ turbo.js
@@ -1,5 +1,13 @@
-let buttonHeader = document.getElementById("buttonHeader");
-console.log(buttonHeader);
-buttonHeader.addEventListener("click", function(){
-    console.log("Is function firing?")
-    this.style.Color = "red"});
+
+let textHeader = document.getElementById("textHeader");
+console.log(textHeader.innerHTML);
+let button1 = document.getElementById("button1");
+function makeSomewhatRed(){
+    if(textHeader.classList.contains("somewhatRed")){
+        textHeader.classList.remove("somewhatRed");
+    }else{
+        textHeader.classList.add("somewhatRed");
+    }
+ }
+ 
+button1.addEventListener("click", makeSomewhatRed);
